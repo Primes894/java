21ff1582a1b6e1c5269319561b0de94ba69b0c1e
Primes894 <primexx427@gmail.com>
2022-12-07 22:22:26 -0500

    Este es

---
+++ Calculadora.java
@@ -0,0 +1,17 @@
+public class Calculadora {
+
+    public static void Escribir(){
+        System.out.print("Holaa");
+    };
+
+    public  static  void EscribirLinea(){
+        System.out.println("Holaa en linea");
+    };
+    public  static  int LeerNumero(int Dato){
+        int Entero=Dato;
+        return  Entero;
+    };
+    public static void main(String[] args) {
+
+    }
+}
